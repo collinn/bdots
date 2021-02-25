773deeff06e86d695eac724cc0cafedcba2af2b2
collin <collin@uiowa.edu>
2021-02-25 16:21:36 -0600

    missed error flags

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Updated 02-23-2021. See NOTES.md for details")
+  packageStartupMessage("Updated 02-25-2021. See NOTES.md for details")
 }
