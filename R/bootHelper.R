62ddafa936325a433bf66b794ba9eb61b7ff85e7
collin <collin@uiowa.edu>
2021-02-27 18:18:36 -0600

    fix documentation, satistfy cran checks, changed arg names for boot to remove .

--- R/bootHelper.R
+++ R/bootHelper.R
@@ -92,6 +92,26 @@ alphaAdjust <- function(curveList, p.adj = "oleson", alpha = 0.05, cores, group 
   list(pval = pval, adjpval = adjpval, alphastar = alphastar, rho = rho)
 }
 
+## Determines time regions where difference significant
+# sig are boolean values
+bucket <- function(sig, time) {
+  if (sum(sig) == 0) return(NULL)
+  rr <- rle(sig)
+  rr$idx <- cumsum(rr$lengths)
+  mm <- rr$values*(rr$idx) + (1 - rr$values)*(rr$idx + 1)
+
+  ## Alt condition if first region true
+  if (rr$values[1] == 1) {
+    #mm <- mm[c(length(mm), 1:(length(mm)-1))]
+    #mm[1] <- 1
+    mm <- c(1, mm)
+  }
+  # If ends on false
+  if (rr$values[length(rr$values)] == 0) {
+    mm <- mm[1:(length(mm) - 1)]
+  }
+  matrix(time[mm], ncol = 2, byrow = TRUE)
+}
 
 
 
